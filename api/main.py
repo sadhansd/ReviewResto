2b19665d4dfcda7594c1f9dddee53b35063cc912
sadhansd <sadhansd02@gmail.com>
2025-01-07 00:22:11 +0530

    add api code

--- api/main.py
+++ api/main.py
@@ -1,9 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from rag import get_suggestions,get_summary,restaurant_qna
 import uvicorn
 import json
+from pydantic import BaseModel
+
+from typing import Optional
+from llama_index.vector_stores.elasticsearch import ElasticsearchStore
+from llama_index.core import Document, Settings, VectorStoreIndex, QueryBundle, StorageContext, load_index_from_storage
+from llama_index.llms.gemini import Gemini
+from llama_index.embeddings.gemini import GeminiEmbedding
+
+API_KEY = 'AIzaSyC2u-JldV2UVXaQHqcXaYWt9zEp3Nk7wa0'
+
+embed_model = GeminiEmbedding(model_name='models/text-embedding-004', api_key=API_KEY)
+Settings.embed_model = embed_model
+
+llm = Gemini(model='models/gemini-1.5-flash', api_key=API_KEY)
+storage_context = StorageContext.from_defaults(persist_dir="notebooks/storage")
+index = load_index_from_storage(storage_context=storage_context, index_id="vector_index")
+query_engine = index.as_query_engine(llm, similarity_top_k=10)
 
 app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query: str
+
+class QueryResponse(BaseModel):
+    response: str
 
 @app.get('/')
 def home():
@@ -23,12 +46,15 @@ async def summarize(restaurant_name: str):
     res = json.loads(res)
     return res
 
-@app.get("/{restaurant_name}")
-async def answer(restaurant_name: str, query: str):
-    res = restaurant_qna(restaurant_name, query)
-    res = res.split('```json')[1].split('```')[0]
-    res = json.loads(res)
-    return res
-
+@app.post("/query")
+async def query_index(request: QueryRequest):
+    try:
+        user_query = request.query
+        bundle = QueryBundle(query_str=user_query, embedding=Settings.embed_model.get_query_embedding(query=user_query))
+        response = query_engine.query(bundle)
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing query: {str(e)}")
+    
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- src/main.py
+++ src/main.py
@@ -10,13 +10,22 @@ st.set_page_config(
 )
 
 st.title("ReviewResto")
-query = st.text_input('Enter your queries', placeholder='eg: Suggest a affordable pizza spot')
+# query = st.text_input('Enter your queries', placeholder='eg: Suggest a affordable pizza spot')
 
-if st.button("Search",icon='🔍'):
-    response = requests.get(f'{API_KEY}/suggest?query={query}')
-    result = response.json()
-    for i in result['recommendations']:
-        st.markdown(f'### {i['restaurant_name']}')
-        st.write(i['review'])
-        st.markdown('---')
-    st.success(result['conclusion'])
+# if st.button("Search",icon='🔍'):
+#     response = requests.get(f'{API_KEY}/suggest?query={query}')
+#     result = response.json()
+#     for i in result['recommendations']:
+#         st.markdown(f'### {i['restaurant_name']}')
+#         st.write(i['review'])
+#         st.markdown('---')
+#     st.success(result['conclusion'])
+
+res_name = st.text_input("Enter restaurant name", placeholder="e.g. Pizza Hut")
+user_query = st.text_input('Enter your queries', placeholder='eg: Suggest a affordable pizza spot')
+if st.button("Submit Query", type='primary'):
+    payload = {"query": user_query+f" in {res_name}"}
+    response = requests.post(f'{API_KEY}/query', json=payload)
+    if response.status_code==200:
+        result = response.json()
+        st.write(result['response'])
